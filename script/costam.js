c423b8ca4ab33cbc5276e5490d2e71c625e70ab3
pamiecwody <pamiecwody@gmail.com>
2020-06-27 15:29:15 +0200

    update page to newer version, add menu

--- script/costam.js
+++ script/costam.js
@@ -1,3 +1,5 @@
+var scrollableSlides = [12, 45, 61, 63];
+
 function resize(haxx) {
   var bodyElement = document.body;
 
@@ -32,6 +34,16 @@ function resize(haxx) {
   bodyElement.style['transform'] = 'translateX(' + xOffset + 'px) translateY(' + yOffset + 'px) scale(' + scale + ')';
 }
 
+function addCssStyle(css) {
+  var element = document.querySelector('style');
+  if (!element) {
+    element = document.createElement('style');
+    element.type = 'text/css';
+    document.querySelector('head').appendChild(element);
+  }
+  element.innerHTML += '\n' + css.trim() + '\n';
+}
+
 function getCurrentSlideFileName() {
   var path = window.location.pathname;
   var pathSplit = path.split("/");
@@ -54,27 +66,159 @@ function switchSlide(offset) {
 }
 
 function slideExist(slideIndex) {
-  return slideIndex > 0 && slideIndex <= 77;
+  return slideIndex > 0 && slideIndex <= 86;
 }
 
-function addHamburgerMenu() {
-  var button = document.createElement('button');
-  button.style['position'] = 'absolute';
-  button.style['top'] = '10px';
-  button.style['left'] = '10px';
-  button.style['background-color'] = '#777';
-  button.style['color'] = 'white';
-  button.style['cursor'] = 'pointer';
-  button.style['padding'] = '5px';
-  button.style['width'] = '30px';
-  button.style['border'] = 'none';
-  button.style['border-radius'] = '3px';
-  button.style['text-align'] = 'center';
-  button.style['outline'] = 'none';
-  button.style['font-size'] = '15px';
+function createElement(parentElement, tagName) {
+  var element = document.createElement(tagName);
+  parentElement.appendChild(element);
+  return element;
+}
+
+
+function isLocal() {
+  if (window.location.href.match('.*/dev/.*')) {
+    return false; //dev mode
+  }
+  return window.location.href.startsWith('file');
+}
+
+function createHamburgerButton(menu) {
+  var button = createElement(document.body.parentElement, 'button');
+  button.className = 'hamburgerButton';
   button.innerText = '☰';
 
-  document.body.parentElement.appendChild(button);
+  menu.style['visibility'] = 'hidden'
+  menu.style['opacity'] = '0'
+  menu.style['transition'] = 'visibility 0.1s linear, opacity 0.1s linear';
+  button.addEventListener("click", function () {
+    if (menu.style['opacity'] == '0') {
+
+      menu.style['visibility'] = '';
+      menu.style['opacity'] = '1';
+    }
+    else {
+      menu.style['opacity'] = '0';
+      menu.style['visibility'] = 'hidden';
+    }
+  });
+}
+
+function createMenu() {
+  var menu = createElement(document.body.parentElement, 'div');
+  menu.className = 'sideMenu';
+  menu.style['visibility'] = 'hidden';
+  menu.style['opacity'] = '0';
+  createHamburgerButton(menu);
+  return menu;
+}
+
+function addMenuHeader(menuElement, text) {
+  var header = createElement(menuElement, 'p');
+  header.className = 'menuHeader';
+  header.innerText = text;
+}
+
+function addMenuFooter(menuElement, name, year) {
+  var footer = createElement(menuElement, 'p');
+  footer.className = 'menuFooter';
+  footer.innerText = name;
+}
+
+function addMenuItem(menuElement, text, url) {
+  var menuItem = createElement(menuElement, 'p');
+  if (url !== undefined) {
+    var link = createElement(menuItem, 'a');
+    link.href = url;
+    link.innerText = text;
+  } else {
+    menuItem.innerText = text;
+  }
+  return menuItem;
+}
+
+function addSideMenu() {
+  var menu = createMenu();
+  addMenuHeader(menu, 'pamięć wody');
+  addMenuItem(menu, 'start', getSlideFileNameForIndex(1));
+  addMenuItem(menu, 'sieci dróg wodnych w Europie', getSlideFileNameForIndex(3));
+  addMenuItem(menu, 'Kanał Mazurski', getSlideFileNameForIndex(10));
+  addMenuItem(menu, 'historia i tożsamość', getSlideFileNameForIndex(25));
+  addMenuItem(menu, 'analiza dostępności', getSlideFileNameForIndex(32));
+  addMenuItem(menu, 'analiza percepcyjna', getSlideFileNameForIndex(37));
+  addMenuItem(menu, 'architektura pogranicza', getSlideFileNameForIndex(43));
+  addMenuItem(menu, 'wnioski', getSlideFileNameForIndex(50));
+  addMenuItem(menu, 'szlak turystyki angażującej', getSlideFileNameForIndex(59));
+  addMenuItem(menu, 'kontakt', getSlideFileNameForIndex(86));
+}
+
+
+function setPageTitle() {
+  document.title = 'Pamięć wody';
+}
+
+function createCssStyles() {
+  addCssStyle(`
+.hamburgerButton {
+  position: fixed;
+  font-size: 24px;
+  top: 5px;
+  left: 5px;
+  background-color: rgb(255, 255, 255, 0.0);
+  cursor: pointer;
+  padding: 5px; 
+  width: 30px;
+  border: none;
+  text-align: center;
+  outline: none;
+  z-index: 1;
+}
+
+.menuFooter {
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  font-family: Myriad Pro;
+  font-weight: light;
+  font-size: 22px;
+}
+
+.menuHeader {
+  position: fixed;
+  font-family: Frank Ruhl Libre Black;
+  font-size: 22px;
+  top: 0px;
+  left: 0px;
+  width: 240px;
+  height: 45px;
+  line-height: 32px;
+  padding: 10px 10px 0px 50px; 
+  background-color: rgb(0, 0, 0, 0.1);
+  border-right: 1px solid rgb(180, 180, 180, 1.0);
+  border-bottom: 1px solid rgb(180, 180, 180, 1.0);
+  z-index: 1;
+}
+
+.sideMenu {
+  font-family: Myriad Pro;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 3.0;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  height: 100%;
+  min-width: 10%;
+  width: 240px;
+  padding: 60px 10px 10px 50px;
+  border-right: 1px solid rgb(180, 180, 180, 1.0);
+  background-color: rgb(255, 255, 255, 0.5);
+  backdrop-filter: blur(7px);
+}
+`);
+  // menu.style['display']='flex';
+  // menu.style['justify-content']='center';
+  // menu.style['align-items']='center';
 }
 
 document.onkeyup = function (e) {
@@ -84,23 +228,22 @@ document.onkeyup = function (e) {
     case 32: // space bar
     case 34: //page down
     case 39: //arrow right
-    case 40: // arrow down
       switchSlide(1);
       break;
     case 33: // page up
     case 37: // arrow left
-    case 38: // arrow up
       switchSlide(-1);
       break;
   }
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
+  setPageTitle();
+  createCssStyles();
   var body = document.body;
   body.style['display'] = 'none';
   body.style['overflow-x'] = 'hidden';
-  var currentSlideIndex = getCurrentSlideIndex();
-  if (currentSlideIndex != 12 && currentSlideIndex != 52 && currentSlideIndex != 54) {
+  if (scrollableSlides.indexOf(getCurrentSlideIndex()) === -1) {
     body.style['overflow-y'] = 'hidden';
   }
 });
@@ -109,7 +252,9 @@ window.addEventListener('load', function (event) {
   document.body.style['display'] = '';
   resize(false);
   window.scrollTo(0, 0);
-  addHamburgerMenu();
+  if (!isLocal()) {
+    addSideMenu();
+  }
 });
 window.addEventListener('resize', function (event) {
   resize(false);
