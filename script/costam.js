ece77e53ead36b01832f91fd94ed0b92c39d5df9
pamiecwody <pamiecwody@gmail.com>
2020-06-27 22:07:41 +0200

    favicon

--- script/costam.js
+++ script/costam.js
@@ -278,8 +278,9 @@ function setPageTitle() {
 function setFavicon() {
   var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
   link.type = 'image/x-icon';
-  link.rel = 'shortcut icon';
+  link.rel = 'icon';
   link.href = 'favicon.ico';
+
   document.getElementsByTagName('head')[0].appendChild(link);
 }
 
