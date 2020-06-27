cb18969af8af8c5ddf809a958c22e5c09ff7f3b2
pamiecwody <pamiecwody@gmail.com>
2020-06-27 21:51:47 +0200

    remove deprecated code

--- script/costam.js
+++ script/costam.js
@@ -477,16 +477,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 window.addEventListener('load', function (event) {
-  // invaliadate cache
-  window.applicationCache.addEventListener('updateready', function (e) {
-    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-      window.applicationCache.swapCache();
-      if (confirm('A new version of this site is available. Load it?')) {
-        window.location.reload();
-      }
-    }
-  }, false);
-
   document.body.style['display'] = '';
   resize(false);
   window.scrollTo(0, 0);
